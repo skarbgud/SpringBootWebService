2b5baa8aa3415e51151b53fc0d71665fc4e0b58a
sjarbgud <sjarbgud@gmail.com>
2021-11-07 00:59:05 +0900

    JPA 테스트 코드 작성 및 Mysql 로그 설정

--- src/main/java/com/gyuhyeong/example/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/gyuhyeong/example/springboot/domain/posts/PostsRepository.java
@@ -3,5 +3,5 @@ package com.gyuhyeong.example.springboot.domain.posts;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 // JpaRepository<Entity 클래스, PK 타입> 상속하면 기본적인 CRUD 메소드 자동 생성
-public interface PostRepository extends JpaRepository<Posts, Long> {
+public interface PostsRepository extends JpaRepository<Posts, Long> {
 }
