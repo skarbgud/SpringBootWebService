2b8489f254e69b4b7e2acbba6522663bbfe32ab0
sjarbgud <sjarbgud@gmail.com>
2021-11-08 21:08:22 +0900

    mustache(부트스트랩, 제이쿼리)를 이용한 등록 화면 구현

--- src/main/java/com/gyuhyeong/example/springboot/web/IndexController.java
+++ src/main/java/com/gyuhyeong/example/springboot/web/IndexController.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-    @GetMapping
+    @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
     }
 }
