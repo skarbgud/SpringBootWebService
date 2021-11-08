5093b0a1b5c1b9124d2e4140271721e9d94ef0d9
sjarbgud <sjarbgud@gmail.com>
2021-11-08 21:36:23 +0900

    글 등록 컨트롤러, 서비스, 레포지토리 구현

--- src/main/java/com/gyuhyeong/example/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/gyuhyeong/example/springboot/domain/posts/PostsRepository.java
@@ -1,7 +1,13 @@
 package com.gyuhyeong.example.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 // JpaRepository<Entity 클래스, PK 타입> 상속하면 기본적인 CRUD 메소드 자동 생성
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/gyuhyeong/example/springboot/service/posts/PostsService.java
+++ src/main/java/com/gyuhyeong/example/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.gyuhyeong.example.springboot.service.posts;
 
 import com.gyuhyeong.example.springboot.domain.posts.Posts;
 import com.gyuhyeong.example.springboot.domain.posts.PostsRepository;
+import com.gyuhyeong.example.springboot.web.dto.PostsListResponseDto;
 import com.gyuhyeong.example.springboot.web.dto.PostsResponseDto;
 import com.gyuhyeong.example.springboot.web.dto.PostsSaveRequestDto;
 import com.gyuhyeong.example.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -36,4 +40,12 @@ public class PostsService {
 
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true) // @Transactional(readOnly = true) => 트랜잭션 범위는 유지하되, 조회 기능만 남겨두어 조회 속도가 개선(등록,수정,삭제 기능이 전혀 없는 곳에서 사용)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)     // 람다식 사용  ===  .map(posts -> new PostsListResponseDto(posts))
+                .collect(Collectors.toList());  // Posts들을 Stream을 map을 통해 PostsListReponseDto 변환 -> List로 반환
+    } 
+    
 }
